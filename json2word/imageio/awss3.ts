de306b260a7f1bc443d33549d71a3ec163ed7368
wbsuperviosr <rabopanjuan@gmail.com>
2023-03-02 21:02:42 +0100

    change awss3.ts setup

--- json2word/imageio/awss3.ts
+++ json2word/imageio/awss3.ts
@@ -1,4 +1,3 @@
-// import S3 from "aws-sdk/clients/s3";
 import { S3 } from "@aws-sdk/client-s3";
 import { keyid, secret, accountid } from "../../credential";
 
@@ -37,23 +36,3 @@ export const downloadImageS3 = async function (
 	const buffer = await obj.Body?.transformToByteArray();
 	fs.writeFileSync(savePath, buffer);
 };
-
-// let key =
-// 	"3观者评说\\Qhja365\\案情推理\\20171213_日本庭审刘鑫和法医回答提问情况对比\\20171213_日本庭审刘鑫和法医回答提问情况对比_01_刘鑫文章《致江女士：言尽于此》_检方要求记不清就说记不清.jpg";
-
-// const run = async () => {
-// 	// const obj = await s3.getObject({
-// 	// 	Bucket: "lxexpress",
-// 	// 	Key: key,
-// 	// });
-// 	const obj = await s3.listObjects({
-// 		Bucket: "lxexpress",
-// 		Prefix: "3观者评说",
-// 	});
-// 	let key1 = key.replaceAll("\\", "/");
-// 	console.log(key1);
-// 	fs.writeFileSync("tmp.json", JSON.stringify(obj.Contents));
-// 	console.log(obj.Contents?.filter((content) => content.Key == key1));
-// };
-
-// run();
